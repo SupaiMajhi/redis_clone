bff43ccff9f06582852ac27133951eccd31a35c3
Supai Majhi <sameertudu978@gmail.com>
2026-04-15 00:02:52 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,7 @@ console.log("Logs from your program will appear here!");
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
     connection.on('data', (data: Buffer) => {
-        const arr = parseRESP(data.toString("utf-8"));
+        const arr = parseRESP(data.toString());
         const val = encodeRESP(arr);
         connection.write(val);       
     });
