e9224cb72634e7cdb06940751552f23e282725b9
Supai Majhi <sameertudu978@gmail.com>
2026-04-14 20:27:13 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -4,8 +4,8 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
-    connection.on('data', (data) => {
-        connection.write('+PONG\r\n');
+    connection.on('data', (data: string) => {
+        console.log(data);
     });
 });
 
