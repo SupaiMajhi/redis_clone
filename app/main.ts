fe8b06dc8977d177a8681657e722a7e39449f739
Supai Majhi <sameertudu978@gmail.com>
2026-04-14 19:50:22 +0530

    give ping's response back

--- app/main.ts
+++ app/main.ts
@@ -4,6 +4,7 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
+    connection.write('+PONG\r\n');
 });
 
 server.listen(6379, "127.0.0.1");
